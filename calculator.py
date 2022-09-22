891f8a4015e3e8017f83ce13340074a365d9b3a3
Evan Jones <evan.jones518@gmail.com>
2022-09-22 18:58:11 -0500

    Added the replace_element function. which takes the original input string and all occurances of the element in question and replaces them with the calculated result.

--- calculator.py
+++ calculator.py
@@ -4,27 +4,50 @@ import math
 """The idea here is to take input and use regexes to put the operations in a workable order according to pemdas."""
 
 def calculate(expression_slice):
+	result = 0
+	expression_slice = re.sub(has_parentheses, '', expression_slice)
+	expression_slice = expression_slice.split()
+
 	if "+" in expression_slice:
-		pass
+		result = float(expression_slice[0]) + float(expression_slice[2])
+		return result
+
 	if "-" in expression_slice:
-		pass
+		result = float(expression_slice[0]) - float(expression_slice[2])
+		return result
+
 	if "*" in expression_slice:
-		pass
+		result = float(expression_slice[0]) * float(expression_slice[2])
+		return result
+
 	if "/" in expression_slice:
-		pass
+		result = float(expression_slice[0]) / float(expression_slice[2])
+		return result
+
+def replace_element(input_expression, all_occurances):
+	for occurance in all_occurances:
+		evaluated = calculate(occurance)
+		input_expression = input_expression.replace(occurance, str(evaluated))
+	return print(input_expression)
+
+
 
 parentheses_pattern = r'(\(\d+ [\+\-\*\/] \d+\))'
 division_pattern = r'(\d+ \/ \d+)'
 multiply_pattern = r'(\d+ \* \d+)'
 addition_pattern = r'(\d+ \+ \d+)'
 subtraction_pattern = r'(\d+ \- \d+)'
+has_parentheses = r'[\(\)]'
 
 print("Welcome to this Regex Calculator!")
 
+expression = input("Enter an expression:\n>>>")
 
-expression = input("Input an expression to be evaluated: \n>>> ")
+occurances = re.findall(parentheses_pattern, expression)
+indices = re.search(parentheses_pattern, expression)
 
+#expression = expression.replace(str(occurances[0]), str(result))
 
-match = re.findall(parentheses_pattern, expression)
+#print(expression)
 
-print(match)
+replace_element(expression, occurances)
